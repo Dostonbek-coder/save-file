a160b2e4c0bc189a753381248ab4203b47992652
Dostonbek-coder <mahmudovdostonbek455@gmail.com>
2025-03-19 10:33:03 +0500

    Controller created to load file

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -1,31 +1,31 @@
 <?php
-// 
-// namespace App\Http\Controllers;
 
-// use App\Models\User;
-// use Illuminate\Http\Request;
-// use App\Http\Controllers\Controller;
+namespace App\Http\Controllers;
 
-// class FileController extends Controller
-// {
-//     public function uploadFile(Request $request) 
-//     { 
-//         if ($request->hasFile('file')) { // tekshiruv file bormi yoqmi
-//             // Fayl obyektini olish 
-//             $file = $request->file('file'); // $request object ichida file metodi bor ichiga inputdagi name ni qabul qiladi 
-//             // Fayl nomini o'zgartirish xar yuklangan file da unique name bo'lishini taminlaydi 
-//             $fileName = time() . '.' . $file->getClientOriginalExtension(); 
-//             // Faylni public diskka yuklash 
-//             $filePath = $file->storeAs('uploads', $fileName, 'public'); 
-//             $user = User::create([
-//                 'name'=>'Dostonbek',
-//                 'eamil'=> uniqid() . '$gmail.com',
-//                 'avatar'=> $filePath,
-//                 'password' => 'password'
-//             ]);
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
-//             return response()->json(['filePath' => $filePath]); 
-//         } 
-//         return 'Fayl yuklanmadi'; 
-//     } 
-// }
+class FileController extends Controller
+{
+    public function uploadFile(Request $request) 
+    { 
+        if ($request->hasFile('file')) { // tekshiruv file bormi yoqmi
+            // Fayl obyektini olish 
+            $file = $request->file('file'); // $request object ichida file metodi bor ichiga inputdagi name ni qabul qiladi 
+            // Fayl nomini o'zgartirish xar yuklangan file da unique name bo'lishini taminlaydi 
+            $fileName = time() . '.' . $file->getClientOriginalExtension(); 
+            // Faylni public diskka yuklash 
+            $filePath = $file->storeAs('uploads', $fileName, 'public'); 
+            $user = User::create([
+                'name'=>'Dostonbek',
+                'eamil'=> uniqid() . '$gmail.com',
+                'avatar'=> $filePath,
+                'password' => 'password'
+            ]);
+
+            return response()->json(['filePath' => $filePath]); 
+        } 
+        return 'Fayl yuklanmadi'; 
+    } 
+}
